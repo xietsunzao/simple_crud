96de1194170c130177dc5d34b8fabfb751348382
xietsunzao <lordshinjo22@gmail.com>
2020-12-04 08:28:56 +0700

    fixed route

---
+++ database/factories/MasterFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Master;
+use Faker\Generator as Faker;
+
+$factory->define(Master::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
